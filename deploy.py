8758a1771a4953d14f65d71209de1c73a8742145
Matt Moore <matt.e.moore@gmail.com>
2018-01-26 10:06:50 -0600

    Added README.md

--- deploy.py
+++ deploy.py
@@ -3,30 +3,6 @@ import sys
 from time import sleep
 import boto3
 
-# What does this do?
-# Test models on your desktop then when you are ready to build the full model, commit script to github and then use 
-# this tool to build the full models using AWS GPU compute instances. 
-# Save time and money - AWS deep learning spot instances are 30% cheaper than on-demand instances.
-# Run 'deploy.py' and walk away, knowing you won't spend any more money than necessary to build your models.
-# This script automatically requests a Deep Learning spot instance, runs a script from source control, archives the output and terminates the instance.
-
-# Requirements:
-# Python 3.6
-# Boto3
-# awscli
-
-# Setup:
-# Run awscli to set keys (need EC2 and S3 permissions) and region
-# Put contents of this repo into root of code to deploy
-# Modify code structure and github settings in 'start.sh' (mandatory)
-# Modify AWS instance settings in 'deploy.py' (not mandatory)
-
-# Usage and Flow:
-# Run 'deploy.py' to create spot instance request
-# When request is fulfilled a Deep Learning instance is created that runs 'start.sh' at bootup
-# After start.sh completes, the instance is shutdown (terminated as it is a spot request)
-# Output (e.g. .pkl files and logs) available in S3 directory
-
 DEEP_LEARN_AMI = 'Deep Learning AMI (Ubuntu) Version 2.0'
 DEEP_LEARN_INSTANCE_TYPE = 'p2.xlarge'
 DEPLOY_TAG = ['Name', 'DeepLearningBox']
