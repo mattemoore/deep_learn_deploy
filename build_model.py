9de78fb4c9337ce9a79934384c02cbc8d05b3aec
Matt Moore <matt.e.moore@gmail.com>
2018-01-30 09:48:46 -0600

    Using own code for build_model.py + more comments for start.sh

--- build_model.py
+++ build_model.py
@@ -1,37 +1,58 @@
-# From: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
-'''Trains a simple convnet on the MNIST dataset.
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
-from __future__ import print_function
+# Basic CNN to classify SVHN dataset using Keras
+import os
+import numpy as np
 import keras
-from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Conv2D, Flatten
+from keras.layers import MaxPooling2D, Dropout
 from keras import backend as K
+from scipy.io import loadmat
 from datetime import datetime
+from urllib.request import urlretrieve
+
+
+def get_data():
+    train_url = 'http://ufldl.stanford.edu/housenumbers/train_32x32.mat'
+    test_url = 'http://ufldl.stanford.edu/housenumbers/test_32x32.mat'
+    train_file = 'dataset/train_32x32.mat'
+    test_file = 'dataset/test_32x32.mat'
+    if not os.path.exists('dataset'):
+        os.makedirs('dataset')
+    with open(train_file, 'w'):
+        pass
+    with open(test_file, 'w'):
+        pass
+    print('Downloading SVHN dataset...')
+    urlretrieve(train_url, train_file)
+    urlretrieve(test_url, test_file)
+    print('Download complete.')
+    train, test = loadmat(train_file), loadmat(test_file)
+    return train, test
+
 
 batch_size = 128
+epochs = 5
 num_classes = 10
-epochs = 12
+img_rows, img_cols = 32, 32
+num_colors = 3
 
-# input image dimensions
-img_rows, img_cols = 28, 28
+train, test = get_data()
+x_train = train['X']
+y_train = train['y'].flatten() - 1
+del train
 
-# the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_test = test['X']
+y_test = test['y'].flatten() - 1
+del test
 
 if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+    x_train = x_train.reshape(x_train.shape[3], num_colors, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[3], num_colors, img_rows, img_cols)
+    input_shape = (num_colors, img_rows, img_cols)
 else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    x_train = x_train.reshape(x_train.shape[3], img_rows, img_cols, num_colors)
+    x_test = x_test.reshape(x_test.shape[3], img_rows, img_cols, num_colors)
+    input_shape = (img_rows, img_cols, num_colors)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -41,34 +62,43 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-# convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3),
+          padding='valid',
+          input_shape=input_shape,
+          activation='relu'))
+model.add(Conv2D(32, (3, 3),
+          activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+# model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
 start = datetime.now()
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
+model.compile(optimizer=keras.optimizers.Adadelta(),
+              loss=keras.losses.categorical_crossentropy,
               metrics=['accuracy'])
-
 model.fit(x_train, y_train,
-          batch_size=batch_size,
           epochs=epochs,
-          verbose=1,
+          batch_size=batch_size,
+          verbose=2,
+          shuffle=True,
           validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
+score = model.evaluate(x_test, y_test, verbose=1)
 end = datetime.now()
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 print('Elapsed time:', end - start)
+
+if not os.path.exists('output'):
+    os.makedirs('output')
+model_file = 'output/model.json'
+weights_file = 'output/model.h5'
+with open(model_file, 'w') as f:
+    f.write(model.to_json())
+model.save_weights(weights_file)
--- start.sh
+++ start.sh
@@ -4,11 +4,27 @@ $GIT_REPO=https://github.com/lushdog/DeepLearning
 $PATH_TO_SCRIPT=DeepLearning/deep_learn_deploy 
 $SCRIPT_NAME=build_model.py
 $OUTPUT_DIR=output
+# for MXNet(+Keras1) with Python3 (CUDA 9) _____________________ source activate mxnet_p36
+# for MXNet(+Keras1) with Python2 (CUDA 9) _____________________ source activate mxnet_p27
+# for TensorFlow(+Keras2) with Python3 (CUDA 8) ________________ source activate tensorflow_p36
+# for TensorFlow(+Keras2) with Python2 (CUDA 8) ________________ source activate tensorflow_p27
+# for Theano(+Keras2) with Python3 (CUDA 9) ____________________ source activate theano_p36
+# for Theano(+Keras2) with Python2 (CUDA 9) ____________________ source activate theano_p27
+# for PyTorch with Python3 (CUDA 8) ____________________________ source activate pytorch_p36
+# for PyTorch with Python2 (CUDA 8) ____________________________ source activate pytorch_p27
+# for CNTK(+Keras2) with Python3 (CUDA 8) ______________________ source activate cntk_p36
+# for CNTK(+Keras2) with Python2 (CUDA 8) ______________________ source activate cntk_p27
+# for Caffe2 with Python2 (CUDA 9) _____________________________ source activate caffe2_p27
+# for base Python2 (CUDA 9) ____________________________________ source activate python2
+# for base Python3 (CUDA 9) ____________________________________ source activate python3
+source activate tensorflow_p36
+
+# Add more Python Packages here as needed by build_model.py
+sudo pip install requests
 
 sudo apt-get update
 sudo apt-get install git -y
 sudo apt-get install awscli -y
-source activate tensorflow_p36
 sudo mkdir deep
 cd deep
 sudo git clone $GIT_REPO
