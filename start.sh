5a335ea10fc498e0d023b27401bbf5a88b939eb8
Matt Moore <matt.e.moore@gmail.com>
2018-02-06 15:13:01 -0600

    Output folder now zipped before sending to S3

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #cloud-boothook
 echo 'RUNNING USER SCRIPT...'
+TIMEZONE=America/Chicago
 GIT_REPO=lushdog/DeepLearning
 PATH_TO_SCRIPT=DeepLearning/deep_learn_deploy 
 SCRIPT_NAME=build_model.py
-OUTPUT_DIR=output
-S3_BUCKET=deep_learning_outputs
+SCRIPT_OUTPUT_DIR=output
+S3_BUCKET=deeplearningoutputs
 DL_ENV=tensorflow_p36
 # for MXNet(+Keras1) with Python3 (CUDA 9) _____________________ mxnet_p36
 # for MXNet(+Keras1) with Python2 (CUDA 9) _____________________ mxnet_p27
@@ -22,14 +23,15 @@ DL_ENV=tensorflow_p36
 # for base Python3 (CUDA 9) ____________________________________ python3
 
 # Add Python Packages here as needed by build_model.py
-# e.g. sudo pip install requests
+# pip install requests
 
-#automatically filled in by deploy.py
+# Filled in by deploy.py
 S3_USER_KEY=[key] 
 S3_SECRET=[secret]
 
-echo 'UPDATING PACKAGES...'
-apt-get update
+echo 'INSTALLING PACKAGES...'
+# apt-get update
+timedatectl set-timezone $TIMEZONE
 apt-get install git -y
 apt-get install awscli -y
 
@@ -39,23 +41,21 @@ mkdir deep
 cd deep
 git clone https://github.com/$GIT_REPO
 cd $PATH_TO_SCRIPT
-mkdir $OUTPUT_DIR
+mkdir $SCRIPT_OUTPUT_DIR
 chown -R ubuntu .
 
-echo 'RUNNING PYTHON DL SCRIPT...'
-# sudo bash -c "source /home/ubuntu/anaconda3/bin/activate $DL_ENV && THEANO_FLAGS=device=cuda python $SCRIPT_NAME > $OUTPUT_DIR/$SCRIPT_NAME.log" -s /bin/sh ubuntu
+echo 'RUNNING PYTHON SCRIPT...'
 su -c "source /home/ubuntu/anaconda3/bin/activate $DL_ENV && THEANO_FLAGS=device=cuda python $SCRIPT_NAME > $OUTPUT_DIR/$SCRIPT_NAME.log" -s /bin/bash ubuntu
-cd $OUTPUT_DIR
 
 echo 'PACKAGING AND UPLOADING OUTPUT...'
-# TODO: finish script output push to S3 bucket
-# sudo cp /var/log/cloud-init-output.log .
-# sudo touch git.log
-# sudo git log --name-status HEAD^..HEAD >> git.log
-# DATE=`date '+%Y-%m-%d-%H.%M.%S'`
-# s3=s3://$GIT_REPO-$DATE
-# sudo aws s3 mb $s3
-# sudo aws s3 cp . $s3  --recursive
+cd $SCRIPT_OUTPUT_DIR
+cp /var/log/cloud*.log .
+git log --name-status HEAD^..HEAD > git.log
+DATE=`date '+%Y-%m-%d-%H.%M.%S'`
+OUTPUT_FILE=output.$DATE.tar.gz
+tar -zcvf "$OUTPUT_FILE" .
+S3_URL=s3://$S3_BUCKET
+aws s3 cp $OUTPUT_FILE $S3_URL -O $S3_USER_KEY -W $S3_SECRET
 
 echo 'SHUTTING DOWN INSTANCE...'
-# sudo shutdown -P
+# shutdown -P
