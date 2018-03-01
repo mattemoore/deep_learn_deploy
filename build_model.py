b34a06e30aeba6bbf7fd11002c93fa640998f6ea
Matt Moore <matt.e.moore@gmail.com>
2018-03-01 08:01:40 -0600

    more tweaks to bundled files and dropout value

--- build_model.py
+++ build_model.py
@@ -27,7 +27,7 @@ def get_data():
 
 
 batch_size = 50
-epochs = 10000
+epochs = 300
 num_classes = 10
 img_rows, img_cols = 32, 32
 num_colors = 3
@@ -78,7 +78,7 @@ model.add(Conv2D(64, (5, 5),
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.3))
+model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
 start = datetime.now()
